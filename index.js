ac4f75ec3b6cab756779a81105beace380735667
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-10-31 19:25:22 -0400

    created questions for engineer

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const pageTemplate = require('./src/page-template');
 // answer to questions 
 const newStaffMember = [];
 //prompt questions via inquirer
+//prompt manager questions
 const managerQuestions = [
     {
       type: 'input',
@@ -35,17 +36,42 @@ const managerQuestions = [
     },
 ];
 
+//prompt user to add another employee
 const  typeOfEmployee = [
     {
       type: 'list',
       message: 'What would you like to do next?',
       name: 'position',
-      choices: ['Create an employee.', 'Create an intern.', 'Finish building my team!'],
+      choices: ['Create an engineer.', 'Create an intern.', 'Finish building my team!'],
     }
 ];
 
+// prompt engineer questions
+const engineerQuestions = [
+    {
+      type: 'input',
+      message: 'What is the engineers name?',
+      name: 'engineerName',
+    },  
+    {
+      type: 'input',
+      message: 'What is the engineers ID number?',
+      name: 'engineerId',
+    }, 
+    {
+      type: 'input',
+      message: 'What is the engineers email?',
+      name: 'engineerEmail',
+    }, 
+    {
+      type: 'input',
+      message: 'What is the engineers github username?',
+      name: 'engineerGithub',
+    },
+]
 
-function askManager() {
+//create manager function
+function createManager() {
   inquirer.prompt(managerQuestions)
   .then(function (answers){
     const Manager = new Manager(
@@ -56,31 +82,46 @@ function askManager() {
     );
     newStaffMember.push(Manager);
     menuQuestions();
-  })};
+})};
 
 // add type of employee
 function menuQuestions() {
-    inquirer.prompt(typeOfEmployee)
-    .then(function(userChoice) {
-        if (userChoice.menu === 'add an engineer') {
-          //prompt engineer questions
-        } else if (userChoice.menu === 'add an intern'){
-         //prompt intern questions
-        } else {
-        // build team function
-        }
+  inquirer.prompt(typeOfEmployee)
+  .then(function(userChoice) {
+    if (userChoice.menu === 'add an engineer') {
+      createEngineer();
+    } else if (userChoice.menu === 'add an intern'){
+     //prompt intern questions
+    } else {
+    // build team function
+    }
     });
-}
+};
+
+// create engineer function
+function createEngineer() {
+  inquirer.prompt(engineerQuestions)
+  .then(function(answers) {
+    const Engineer = new Engineer(
+      answers.engineerName, 
+      answers.engineerId, 
+      answers.engineerEmail, 
+      answers.engineerGithub
+      );
+    newStaffMember.push(Engineer);
+    menuQuestions();
+    });
+};
 
 
 
-  //if the manager wants to add an employee 
-  if (answers.position === 'Create an employee.'){
+//if the manager wants to add an employee 
+if (answers.position === 'Create an employee.'){
 
-  } else if (questions.position === 'Create an intern.'){
+} else if (questions.position === 'Create an intern.'){
 
-  } else if (questions.position === 'Finish building my team!') {
+} else if (questions.position === 'Finish building my team!') {
 
-  };
+};
 
-  askManager();
+  createManager();
