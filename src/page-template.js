ab80650b31204ddc25030890442fd61eafc75591
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-10-31 20:11:41 -0400

    began working on page template to render engineer card

--- src/page-template.js
+++ src/page-template.js
@@ -4,14 +4,13 @@ function generateSite(team) {
 const html = [];
 
 //generate manager html
-function generateManager(manager) {
+function renderManager(manager) {
  let managerHtml = 
  `
   <div class="card col-9 col-lg-3">
     <div class="card-header">
       ${manager.getName()}, 
       ${manager.getRole()}
-     <i class="fa-solid fa-people-roof"></i>
     </div>
     <div class="card-body">
       <ul class="list-group">
@@ -24,4 +23,26 @@ function generateManager(manager) {
 `;
 html.push(managerHtml);
 };
+
+// function to generate engineer html
+function renderEngineer(engineer) {
+let engineerHtml = 
+`
+  <div class="card col-9 col-lg-3">    
+    <div class="card-header">
+      ${engineer.getName()}, 
+      ${engineer.getRole()}
+    </div>
+    <div class="card-body">
+      <ul class="list-group">
+        <li class="list-group-item">ID: ${engineer.getId()}</li>
+        <li class="list-group-item">Email: <span id='email'><a href='mailto:${engineer.getEmail()}'> ${engineer.getEmail()}</a></span></li>
+        <li class="list-group-item">GitHub: <span id='email'><a target='_blank' href='https://github.com/${engineer.getGithub()}'> ${engineer.getGithub()}</a></span></li>
+      </ul>
+    </div>
+  </div>
+`;
+html.push(engineerHtml);
+}
+
 };
